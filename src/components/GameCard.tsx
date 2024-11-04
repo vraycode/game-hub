755bf353f62b029b7334a16a4be4f63204533d94
vraycode <18143101820@163.com>
2024-11-04 16:38:48 +0000

    build gamecard.

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,20 @@
+import { Card, CardBody, Image, Heading } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    //@ts-ignore
+    <Card borderRadius={10} overflow={"hidden"}>
+      <Image src={game.background_image}></Image>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
