dfb293acf945890fff658c90e6aa110c0a066552
vraycode <18143101820@163.com>
2024-11-10 16:50:53 +0000

    optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Image, Heading, Text, HStack } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import Metacritic from "./Metacritic";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -11,7 +12,7 @@ const GameCard = ({ game }: Props) => {
   return (
     //@ts-ignore
     <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image}></Image>
+      <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
