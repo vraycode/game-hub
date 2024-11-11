41642caaf97a51c27201a7d059e593e8c5445a1f
vraycode <18143101820@163.com>
2024-11-11 22:15:41 +0000

    display genre image

--- src/components/GenereList.tsx
+++ src/components/GenereList.tsx
@@ -1,15 +1,26 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGeneres from "../hooks/useGeneres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenereList = () => {
   const { data } = useGeneres();
 
   return (
     // @ts-ignore
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={"5px"}>
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            ></Image>
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
